0fac1e163ec7b676af34b50cdd6bc42d294b9903
Basmalakh22 <basmalakha772@gmail.com>
2024-09-28 08:17:37 +0300

    layouts

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('dashboard/index',[DashboardController::class,'index']);
