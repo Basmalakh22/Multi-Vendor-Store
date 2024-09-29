ea6d865bcfcd37e84e1d3277ead0a903522ef182
Basmalakh22 <basmalakha772@gmail.com>
2024-09-29 11:40:12 +0300

    authentication

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
 
+    public function __construct(){
+        // $this->middleware(middleware: ['auth']);
+    }
 
     public function index(){
+
+        $user = Auth::user();
         return view('dashboard.index',[
             'user' => 'Basmala Khaled'
         ]);
